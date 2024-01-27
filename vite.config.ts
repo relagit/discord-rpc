9dc7fc411f706103f95718ca70e3896d0a8b2b9a
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-01-27 17:35:21 +1100

    (package) use @relagit/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import { defineConfig } from "vite";
-
-import { builtinModules } from "node:module";
+import { defineConfig } from "@relagit/vite";
 
 export default defineConfig({
-    build: {
-        target: "esnext",
-        outDir: "dist",
-        emptyOutDir: true,
-        lib: {
-            entry: ["src/plugin.ts", "src/native.ts"],
-            formats: ["es"],
-            fileName: (_, name) => `${name}.js`,
-        },
-        rollupOptions: {
-            external: builtinModules.concat("relagit:actions", "node:crypto", "node:fs", "node:path", "node:events", "node:net"),
-        },
+    entrypoints: {
+        plugin: "src/plugin.ts",
+        native: "src/native.ts",
     },
 });
